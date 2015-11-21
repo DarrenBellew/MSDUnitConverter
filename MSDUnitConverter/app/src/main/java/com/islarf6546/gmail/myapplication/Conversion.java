40cb1f59a6165d4096a6ede673f1fbc760b5dab4
islarf <islarf6546@gmail.com>
2015-11-21 22:45:30 +0000

    Pre-test on new conversion setup, xml not updated

--- MSDUnitConverter/app/src/main/java/com/islarf6546/gmail/myapplication/Conversion.java
+++ MSDUnitConverter/app/src/main/java/com/islarf6546/gmail/myapplication/Conversion.java
@@ -3,18 +3,13 @@ package com.islarf6546.gmail.myapplication;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,12 +17,8 @@ public class Conversion extends Activity {
 
     EditText value1;
     EditText value2;
-    Button swapConversion;
-    Button calculate;
-    TextView answer;
 
-    Map<String, String> conversionData = new HashMap<String, String>();
-    boolean convSwitched = false;
+    Map<String, String> conversionData = new HashMap<>();
     String valName1;
     String valName2;
     @Override
@@ -38,17 +29,15 @@ public class Conversion extends Activity {
         valName1 = getIntent().getExtras().getString("unit1Name");
         valName2 = getIntent().getExtras().getString("unit2Name");
         value1 = (EditText) findViewById(R.id.edit_text_value_1);
-        value2 = (EditText) findViewById(R.id.edit_text_value_2);
-        value2.setFocusable(false);
         value1.addTextChangedListener(new TextWatcher(){
             @Override
-            public void afterTextChanged(Editable arg0)  {
+            public void afterTextChanged(Editable s)  {
             }
             @Override
-            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3)  {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)  {
             }
             @Override
-            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3)  {
+            public void onTextChanged(CharSequence s, int start, int before, int arg3)  {
                 float value;
                 String text = value1.getText().toString();
                 value = Float.valueOf(text);
@@ -58,9 +47,29 @@ public class Conversion extends Activity {
                 value2.setText(Double.toString(m.eval()));
             }
         });
-        swapConversion = (Button) findViewById(R.id.button_swap_conversion);
-        calculate = (Button) findViewById(R.id.button_calculate);
-        answer = (TextView) findViewById(R.id.text_view_answer);
+        value2 = (EditText) findViewById(R.id.edit_text_value_2);
+        value2.setFocusable(false);
+        value2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                float value;
+                String text = value1.getText().toString();
+                value = Float.valueOf(text);
+                MathsParser m;
+                String form = conversionData.get("toFormula");
+                m = new MathsParser(form, value);
+                value2.setText(Double.toString(m.eval()));
+            }
+        });
+
 
 
         Cursor c;
@@ -79,34 +88,6 @@ public class Conversion extends Activity {
             finish();
         }
 
-
-
-        swapConversion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //already switched, setting back
-                if(convSwitched)  {
-                    value2.setFocusableInTouchMode(false);
-                    value1.setFocusable(true);
-                }
-                //not switched, making switched
-                else  {
-                    value2.setFocusable(true);
-                    value1.setFocusableInTouchMode(false);
-                }
-                //changing the boolean
-                convSwitched = !convSwitched;
-
-            }
-        });
-
-        calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
     }
 
     @Override
